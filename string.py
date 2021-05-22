a2d2add5a22e4017b4b21dac9648db93f4669448
mohammedismayil <mohammedismayil10@gmail.com>
2021-05-22 23:01:35 +0530

    variable and string files added

---
+++ string.py
@@ -0,0 +1,12 @@
+
+
+name = 'ismayil'
+
+age = 24
+# Concatenate
+print('hello'+ name + 'your age is ' + str(age))
+
+
+# by arguement
+
+print('hello {name} what are you doing and your age is {age}'.format(name=name,age = age))
---
+++ variable.py
@@ -0,0 +1,26 @@
+print("as usual hello world")
+
+# enter in terminal - python3 variable.py
+'''checking multi line
+comment
+with this 3 line comment'''
+x = '50.333333';
+y = 55;
+z = True;
+z= not z;
+y = 100;
+print(type(y))
+print(type(x))
+print(float(x)+int(y))
+
+print(z)
+
+
+fruits = ["apple", "banana", "cherry","orange"]
+x, y, z ,k  = fruits
+print(x)
+print(y)
+print(z)
+
+
+
