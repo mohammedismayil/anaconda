b2241c2dd90191e79e67fde0a9f9458af3f008a6
mohammedismayil <mohammedismayil10@gmail.com>
2021-07-22 22:43:05 +0530

    empty mutation

--- list-comprehension.py
+++ list-comprehension.py
@@ -1,9 +1,32 @@
-if __name__ == '__main__':
+from itertools import permutations
+ 
+
+# if __name__ == '__main__':
     # x = int(input())
     # y = int(input())
     # z = int(input())
     # n = int(input())
-    fruits = ["apple","orange","strawberry","pineapple"]
-    newlist = [a for a in fruits]
-    print(newlist)
+    # fruits = ["apple","orange","strawberry","pineapple"]
+    # newlist = [a  if a != "apple" else "salad" for a in fruits]
+    
+    # Get all permutations of [1, 2, 3]
+# perm = permutations([0,x, y, z])
+# print(list(perm))
+# Print the obtained permutations
+arr = []
+perm = permutations([1,1,2])
+# print(list(perm))
+for i in list(perm):
+    sum = 0
+    # print(i)
+    for j in i:
+        print(j)
+        sum = sum + j 
+        if sum < 10000:
+         arr.append(i)
+            
+   
+print(arr)
+
+
 
