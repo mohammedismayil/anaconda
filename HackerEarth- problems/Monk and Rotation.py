42509136bbd749e6e996088526af2d09a818f7bb
mohammedismayil <mohammedismayil10@gmail.com>
2021-08-03 07:54:11 +0530

    added anotheer way of solution

--- HackerEarth- problems/Monk and Rotation.py
+++ HackerEarth- problems/Monk and Rotation.py
@@ -1,24 +1,21 @@
-testCase = 4
-
 testCase = input()
-numberOfElements,numberOfSkips =input().split()
-numbersArr = input()
-numberArray = list(map(int,numbersArr.split(' ')))
-newArr = []
-for i in range(len(numberArray)):
-    if i <= (len(numberArray) - 1) :
-     newArr.append(numberArray[i - int(numberOfSkips)])  
-for i in newArr:
-    print(i, end=" ")
+for _ in range(len(testCase)):
+    numberOfElements,numberOfSkips =input().split()
+    # numbersArr = input()
+    numberArray = list(map(int,input().split(' ')))
+    newArr = []
+    for i in range(len(numberArray)):
+     if i <= (len(numberArray)) :
+         if int(numberOfSkips) >  (len(numberArray)) :
+          newArr.append(numberArray[i + int(numberOfSkips)])
+         else:
+              newArr.append(numberArray[i - int(numberOfSkips)])
+    for i in newArr:
+      print(i, end=" ")
 
-
-
-
-
-
-
-
-
-
-
-
+# testCase  = int(input())
+# for _ in range(testCase):
+#     n,k = map(int,input().split())
+#     l = list(map(int,input().split()))
+#     x = k%n
+#     print(*(l[n-x:]+l[:n-x]))
