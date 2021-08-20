f4359abead7f1b23ee1a3b304f5967a412db59a5
ismayil <mohammedismayil10@gmail.com>
2021-08-20 09:22:39 +0530

    initial number finding

---
+++ Ownproblems/descbackup.py
@@ -0,0 +1,51 @@
+# arr = [1,4,6,7,2,10,22,89,5,90]
+
+# # finalarr = []
+# # # for i in range(0,len(arr)):
+# # #      for j in range(0,len(arr)):
+# # #          if i != len(arr) - 1 and j != len(arr) - 1:
+# # #           if arr[i] > arr[j]:
+# # #             tempvalue = arr[i]
+            
+# # #           else:
+# # #             tempvalue = arr[j]
+
+
+# # for j in range(0,len(arr) - 1):
+
+
+# #     for i in range(0,len(arr) - 1):
+# #       if arr[i] > arr[i + 1]:
+# #         tempValue = arr[i]
+# #       else:
+# #          tempvalue = arr[i+1] 
+
+
+# #     finalarr.append(tempvalue)       
+# #     #  arr.remove(tempvalue)
+# #     arr.remove(tempvalue)
+    
+
+# # print(arr)
+# # print(finalarr)
+             
+
+# def descendingOrder(array):
+#     descendingArr = []
+#     for j in range(0,len(array) - 1):
+
+
+#      for i in range(0,len(array) - 1):
+#       if array[i] > array[i + 1]:
+#         tempValue = array[i]
+#       else:
+#          tempvalue = array[i+1] 
+
+
+#     descendingArr.append(tempvalue)       
+#     #  arr.remove(tempvalue)
+#     array.remove(tempvalue)
+#     print(descendingArr)
+#     return descendingArr
+
+# descendingOrder([1,4,6,7,2,10,22,89,5,90])
--- Ownproblems/descending.py
+++ Ownproblems/descending.py
@@ -1,32 +1,49 @@
-arr = [1,4,6,7,2,10,22,89,5,90]
+arr = [112,189,1,4,6,7,2,10,22,223,89,5,90]
 
 finalarr = []
-# for i in range(0,len(arr)):
-#      for j in range(0,len(arr)):
-#          if i != len(arr) - 1 and j != len(arr) - 1:
-#           if arr[i] > arr[j]:
-#             tempvalue = arr[i]
-            
-#           else:
-#             tempvalue = arr[j]
+tempvalue = 0
+# for j in range(0,len(arr) - 1):
 
 
-for j in range(0,len(arr) - 1):
+#     for i in range(0,len(arr) - 1):
+#       if i <= len(arr) - 1 and j <= len(arr) - 1:
+#        if arr[i] > arr[j+1]:
+#         tempValue = arr[i]
+#         # arr.remove(arr[j])
+#         # finalarr.append(tempvalue)
+#        else:
+#          tempvalue = arr[j+1] 
+#         #  arr.remove(arr[i])
+#         #  finalarr.append(tempvalue)
 
 
-    for i in range(0,len(arr) - 1):
-      if arr[i] > arr[i + 1]:
-        tempValue = arr[i]
-      else:
-         tempvalue = arr[i+1] 
-
-
-    finalarr.append(tempvalue)       
-    #  arr.remove(tempvalue)
-    arr.remove(tempvalue)
+#       print(tempvalue)
     
+    
+#     # finalarr.remove(tempvalue)    
+      
+#     #  arr.remove(tempvalue)
+#     # arr.remove(tempvalue)
+    
+for k in range(0,len(arr) - 1):
+ for i in range(0,len(arr) - 1):
+    for j in range(0,len(arr) - 1):
+      if i <= len(arr) - 1 and j <= len(arr) - 1 :
+       print('iteration',i,j,k)
+       if arr[i] > arr[j+1] and arr[i] > tempvalue:
+        tempValue = arr[i]
+        # print(tempvalue)
+        # arr.remove(arr[j])
+        # finalarr.append(tempvalue)
+       elif arr[j+1] > tempvalue:
+         tempvalue = arr[j+1] 
+        #  print(tempvalue)
+    arr.remove(tempvalue)
+ 
+ finalarr.append(tempvalue)
 
-print(arr)
+
+print(tempvalue)
 print(finalarr)
              
 
