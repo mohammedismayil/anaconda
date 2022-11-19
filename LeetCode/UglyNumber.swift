b01ad5bc9cc625dd51960a612feff65ee701ef19
Mohammed Ismayil <mohammedismayil10@gmail.com>
2022-11-19 09:53:41 +0530

    completed ugly number problem

--- LeetCode/UglyNumber.swift
+++ LeetCode/UglyNumber.swift
@@ -1,21 +1,12 @@
-class Solution {
-    func isUgly(_ n: Int) -> Bool {
-        if n == 1 || n % 5 == 0 {
-            return true
-        }else{
-            var final = false
-            for i in 1..<n{
-                
-                if n % i == 0 && i != n && ((n / i ) > 1)  {
-                  final = false
-                  
-                }else if (i != n && n % 2 != 0){
-                    final = true
-                    
-                }
 
-            }
-            return final
-        }
+// https://leetcode.com/problems/ugly-number/submissions/846153699/
+class Solution {
+     func isUgly(_ num: Int) -> Bool {
+        var input = num
+        if input <= 0 { return false }
+        while input % 5 == 0 { input /= 5 }
+        while input % 3 == 0 { input /= 3 }
+        while input % 2 == 0 { input /= 2 }
+        return input == 1 ? true : false
     }
 }
