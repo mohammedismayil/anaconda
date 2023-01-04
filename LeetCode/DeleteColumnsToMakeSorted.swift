646ec3eadd63bd04985bffd79f3b6e4eaff4bb64
Mohammed Ismayil <mohammedismayil10@gmail.com>
2023-01-04 22:52:22 +0530

    added jan 3 leetcode problem

---
+++ LeetCode/DeleteColumnsToMakeSorted.swift
@@ -0,0 +1,34 @@
+class Solution {
+    func minDeletionSize(_ strs: [String]) -> Int {
+     
+        
+        var arr = strs.map({Array($0)})
+        
+        print(arr)
+        var count = 0
+        for i in 0..<arr[0].count{
+            
+            
+            for j in 1..<arr.count{
+
+                
+                if arr[j][i] < arr[j-1][i]{
+                    count += 1
+                    
+                    break
+                }
+                
+                
+
+            }
+            
+        }
+       
+        
+        
+        
+        
+        return count
+        
+    }
+}
